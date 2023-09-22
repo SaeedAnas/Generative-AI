eae7a77b2a991e0dc6901ac5322807cef625fbeb
praveenhm <praveenhm2@yahoo.com>
2023-09-22 16:25:35 -0700

    updates to remove json

--- Semanticsearch/src/app.py
+++ Semanticsearch/src/app.py
@@ -8,6 +8,9 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import List
 from fastapi.responses import FileResponse
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+from fastapi import Response
 
 class SearchResult(BaseModel):
     text: str
@@ -15,14 +18,20 @@ class SearchResult(BaseModel):
 
 app = FastAPI()
 
+STATIC_DIR="/Users/praveen/dev/project-SV/Assignment1/github/Generative-AI/Semanticsearch/src"
+
 # Serve static files from the 'static' directory
+#app.mount("/static", StaticFiles(directory="/Users/praveen/dev/project-SV/Assignment1/github/Generative-AI/Semanticsearch/src/static"), name="static")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 def root_endpoint():
+    #return FileResponse("/Users/praveen/dev/project-SV/Assignment1/github/Generative-AI/Semanticsearch/src/static/search.html")
     return FileResponse("static/search.html")
 
-@app.post("/search", response_model=List[SearchResult])
+
+@app.post("/search")
 def search_endpoint(request: Request, query: str = Body(...), top_k: int = 10, rerank_k: int = 5, search_type: Optional[str] = Body("text")):
 
     try:
@@ -31,8 +40,11 @@ def search_endpoint(request: Request, query: str = Body(...), top_k: int = 10, r
         results = search(query, top_k, rerank_k,search_type=search_type)
        
         # Create the required response structure and return as JSON
-        response_data = {"result": results}
-        return JSONResponse(content=response_data)
+        response_data = {'result': results}
+        return response_data
+      
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
