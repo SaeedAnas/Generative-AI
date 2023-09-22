356e306464b0a22c48cad3f72f3bf2182bdc2abd
praveenhm <praveenhm2@yahoo.com>
2023-09-22 12:13:15 -0700

    updates new search logic

--- Semanticsearch/src/SSearch3_searcher.py
+++ Semanticsearch/src/SSearch3_searcher.py
@@ -5,7 +5,7 @@ import numpy as np
 from elasticsearch import Elasticsearch, helpers
 from sentence_transformers import SentenceTransformer, CrossEncoder
 from helpers.log_utils import setup_logger
-import os
+import os, sys
 
 # Set up the logger
 logger = setup_logger()
@@ -16,8 +16,7 @@ ELASTIC_URL = os.getenv("ELASTIC_URL")
 ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
 ELASTIC_CERT_PATH = os.getenv("ELASTIC_CERT_PATH")
 
-MODEL_SBERT_768 = os.environ['MODEL_SBERT_768'] 
-MODEL_SBERT_384 = os.environ['MODEL_SBERT_384'] 
+MODEL_SBERT = os.environ['MODEL_SBERT'] 
 
 # Create the client instance
 es = Elasticsearch(
@@ -27,7 +26,7 @@ es = Elasticsearch(
 )
 
 # FAISS and Transformer setup
-DIMENSION = 768
+DIMENSION = 768 #os.environ['DIMENSIONS']
 try:
     faiss_index = faiss.read_index('faiss_index.index')
     logger.info(f"Number of vectors in FAISS index: {faiss_index.ntotal}")
@@ -39,12 +38,13 @@ bi_encoder = None
 cross_encoder = None
 try:
     bi_encoder = SentenceTransformer('sentence-transformers/paraphrase-distilroberta-base-v1')
-    cross_encoder = CrossEncoder(MODEL_SBERT_768)
+    cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2')
 except Exception as e:
     logger.error(f"Error loading models: {e}")
     exit(1)  # Exit if cannot load the models
 
-def search(query, top_k=10, rerank_k=5):
+
+def search(query, top_k=10, rerank_k=5,search_type='text'):
     """Execute the hybrid search with re-ranking."""
     if not query:
         logger.warning("Received an empty query. Aborting search.")
@@ -64,7 +64,7 @@ def search(query, top_k=10, rerank_k=5):
         }
 
         es_results = es.search(index="document_index", body=es_search)
-        candidate_sentences = [hit["_source"]["text"] for hit in es_results["hits"]["hits"]]
+        lexical_hits = [hit["_source"]["text"] for hit in es_results["hits"]["hits"]]
 
     except Exception as e:
         logger.error(f"Error during Elasticsearch search: {e}")
@@ -72,52 +72,42 @@ def search(query, top_k=10, rerank_k=5):
 
     # Step 2: Semantic Search with FAISS
     try:
-        # Encode candidate sentences and query
-        candidate_embeddings = bi_encoder.encode(candidate_sentences, convert_to_numpy=True)
         query_vector = bi_encoder.encode(query, convert_to_numpy=True)
-
-        # Get closest vectors to the query
         _, faiss_indices = faiss_index.search(query_vector.reshape(1, DIMENSION), top_k)
-        
-        # Ensure we don't exceed the length of candidate_sentences
-        max_index = len(candidate_sentences) - 1
-        capped_indices = [i if i <= max_index else max_index for i in faiss_indices[0]]
-
-        semantic_hits = [candidate_sentences[i] for i in capped_indices]
+        semantic_hits = [es.get(index="document_index", id=index_id)["_source"]["text"] for index_id in faiss_indices[0]]
 
     except Exception as e:
         logger.error(f"Error during FAISS search: {e}")
         return []
 
-    # Step 3: Re-ranking with Cross-encoder
+    # Step 3: Combine Results
+    combined_hits = list(set(semantic_hits[:top_k] + lexical_hits[:top_k]))
+
+    # Step 4: Re-ranking with Cross-encoder
     try:
-        pairs = [(query, hit) for hit in semantic_hits]
+        pairs = [(query, hit) for hit in combined_hits]
         scores = cross_encoder.predict(pairs)
 
         # Sort by scores and select top rerank_k results
-        sorted_results = sorted(zip(semantic_hits, scores), key=lambda x: x[1], reverse=True)[:rerank_k]
-
+        sorted_results = sorted(zip(combined_hits, scores), key=lambda x: x[1], reverse=True)[:rerank_k]
+      
         logger.info(f"Found {len(sorted_results)} relevant results for query: {query}")
-        return sorted_results
+
+        # Check if we are in the context of an API request or standalone execution
+        if 'fastapi' in sys.modules:
+            # Return as a list of dictionaries for API compatibility
+            #return [{"text": result["_source"]["text"], "score": result["score"]} for result in sorted_results]
+            formatted_results = [{"text": result[0], "score": result[1]} for result in sorted_results]
+            return {"result": formatted_results}
+        else:
+            # Return as a list of tuples for standalone compatibility
+            #return [(result["_source"]["text"], result["score"]) for result in sorted_results]
+            return [(text, score) for text, score in sorted_results]
 
     except Exception as e:
         logger.error(f"Error during re-ranking: {e}")
         return []
 
-
-if __name__ == "__main__":
-    query = input("Enter your query: ").strip()
-    if not query:
-        logger.warning("Empty search query received.")
-        exit(0)  # End the program if the query is empty
-
-    search_results = search(query)
-
-    # Printing the search results 
-    for rank, (text, score) in enumerate(search_results, 1):
-        print(f"Rank: {rank}, Score: {score:.4f}")
-        print(text)
-        print("-" * 80)
 
 
 if __name__ == "__main__":
