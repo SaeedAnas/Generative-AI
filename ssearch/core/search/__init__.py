41079b6b898109eac9056462038685c7d9fdfc08
Anas Saeed <saeedanas396@gmail.com>
2023-11-18 15:45:26 -0800

    new repo for assistant

--- ssearch/core/search/__init__.py
+++
@@ -1,3 +0,0 @@
-from es import search_es
-from vector import search_vector
-from hybrid import search_hybrid
--- ssearch/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-        name = "ssearch",
-        version = "0.0.1",
-        author = "Andes Mountes",
-        packages = find_packages()
-)
-
